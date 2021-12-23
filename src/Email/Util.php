b7418a2cc916a533e9f422fefe0bbbf41cd5faed
Guilherme Ferreira <guifcosta.contato@gmail.com>
2021-12-23 15:18:27 -0300

    Mandando alteracoes

---
+++ src/Email/Util.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ferreira\CursoComposer\Email;
+
+use PHP_CodeSniffer\Tokenizers\PHP;
+
+class Mat
+{
+    public $id = 0;
+    public $name = "random name";
+
+    public function abc() {
+        echo "abc" . PHP_EOL;
+    }
+
+}
+
+?>
+
